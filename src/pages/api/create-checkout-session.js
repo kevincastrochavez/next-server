7bad8cfb7ee84b66cded4592ada2e8f77e99e3da
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-06 13:39:07 -0600

    first commit

---
+++ src/pages/api/create-checkout-session.js
@@ -0,0 +1,78 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async (req, res) => {
+  const { items } = req.body;
+
+  console.log(items);
+
+  const transformedItems = items.map((item) => ({
+    quantity: 1,
+    price_data: {
+      currency: 'usd',
+      unit_amount: item.price * 100,
+      product_data: {
+        name: item.title,
+        images: [item.image],
+      },
+    },
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 399,
+            currency: 'usd',
+          },
+          display_name: 'Next-day',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 5,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 0,
+            currency: 'usd',
+          },
+          display_name: 'Free Delivery',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 12,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 15,
+            },
+          },
+        },
+      },
+    ],
+    shipping_address_collection: {
+      allowed_countries: ['US'],
+    },
+    line_items: transformedItems,
+    mode: 'payment',
+    success_url: `${process.env.HOST}/success`,
+    cancel_url: `${process.env.HOST}/checkout`,
+    metadata: {
+      email: 'kevin',
+      images: JSON.stringify(items.map((item) => item.image)),
+    },
+  });
+
+  res.status(200).json({ id: session.id });
+};
---
+++ src/pages/api/webhook.js
@@ -0,0 +1,70 @@
+import { buffer } from 'micro';
+import * as admin from 'firebase-admin';
+
+const serviceAccounts = require('../../../permissions.json');
+
+const app = !admin.apps.length
+  ? admin.initializeApp({ credential: admin.credential.cert(serviceAccounts) })
+  : admin.app();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+const fullfillOrder = async (session) => {
+  // console.log(session);
+
+  return app
+    .firestore()
+    .collection('users')
+    .doc(session.metadata.email)
+    .collection('orders')
+    .doc(session.id)
+    .set({
+      amount: session.amount_total / 100,
+      amount_shipping: session.total_details.amount_shipping / 100,
+      images: JSON.parse(session.metadata.images),
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+      console.log(`SUCCESS: Order ${session.id} has been added to the db`);
+    });
+};
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const requestBuffer = await buffer(req);
+    const payload = requestBuffer.toString();
+    const signature = req.headers['stripe-signature'];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        payload,
+        signature,
+        endpointSecret
+      );
+    } catch (error) {
+      console.warn(error.message);
+      return res.status(400).send(`Webhook error: ${error.message}`);
+    }
+
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object;
+
+      return fullfillOrder(session)
+        .then(() => res.status(200))
+        .catch((error) =>
+          res.status(400).send(`Webhook error: ${error.message}`)
+        );
+    }
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
